33f73eb08edf1b4cd81fbab2df911dce730ed95b
Hackers Account <barasam07@gmail.com>
2025-03-05 16:10:40 +0300

    Continuation to RESTFULAPIS

--- watchmate/watchlist_app/urls.py
+++ watchmate/watchlist_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from watchlist_app.views import movie_list
+from watchlist_app.views import movie_list, movie_detail
 
 
 urlpatterns = [
     path('list/', movie_list, name='movie_list'),
+    path('<int:pk>/', movie_detail, name='movie_detail'),
+    # path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
+    # path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='stream-detail'),
 ]
